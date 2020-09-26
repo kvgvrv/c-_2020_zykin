f293e154e8438a62969a95ab6c090009e804e8c2
kvgvrv <71896722+kvgvrv@users.noreply.github.com>
2020-09-26 20:49:14 +0300

    Update itmipt.cpp

--- itmipt.cpp
+++ itmipt.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 
 
-
 class Fraction {
 
 private:
 
-	uint64_t gd(int64_t a, uint64_t b) {
-		a = abs(a);							//если че мб надо помен€ть uin64_t на инт и добавить
-		if (a >= b) {					   // вз€тие модул€
+	uint64_t gd(uint64_t a, uint64_t b) {
+		a = abs(a);
+		if (a >= b) {
 			if (a % b == 0) {
 				return b;
 			}
@@ -40,8 +39,10 @@ private:
 
 
 public:
+
 	int64_t numerator;
 	uint64_t denominator;
+
 	Fraction() = delete;
 	Fraction(const Fraction& rhs) {
 		numerator = rhs.numerator;
@@ -67,6 +68,11 @@ public:
 		}
 	}
 
+	~Fraction() {
+		delete &numerator;
+		delete &denominator;
+	}
+
 	Fraction operator+(Fraction rhs) {
 		Fraction timev = *this;
 		if (timev.denominator == rhs.denominator) {
@@ -90,7 +96,7 @@ public:
 		}
 		else {
 			uint64_t tmp = gcd(timev.denominator, rhs.denominator);
-				timev.numerator *= tmp / denominator;
+				timev.numerator *= tmp / timev.denominator;
 				rhs.numerator *= tmp / rhs.denominator;
 				timev.numerator -= rhs.numerator;
 				timev.denominator = tmp;
@@ -107,38 +113,46 @@ public:
 		return timev;
 	}
 
-//	Fraction operator/(Fraction rhs) {
-//		if (rhs.numerator != 0) {
-//			Fraction timev = *this;
-//			if (rhs.numerator < 0) {
-//				timev.numerator *= (-1) * rhs.denominator;
-//				timev.denominator *= (-1) * rhs.numerator;
-//			}
-//			else {
-//				timev.numerator *= rhs.denominator;
-//				timev.denominator *= rhs.numerator;
-//			}
-//			Reduct(timev);
-//			return timev;
-//		}
-//	}
-
 	Fraction operator-() {
 		Fraction timev = *this;
 		timev.numerator *= -1;
 		return timev;
 	}
-	~Fraction() {
-		delete &numerator;
-		delete &denominator;
+
+	Fraction& operator+=(Fraction rhs) {
+		if (denominator == rhs.denominator) {
+			numerator += rhs.numerator;
+		}
+		else {
+			uint64_t tmp = gcd(denominator, rhs.denominator);
+				numerator *= tmp / denominator;
+				rhs.numerator *= tmp / rhs.denominator;
+				numerator += rhs.numerator;
+				denominator = tmp;
+			}
+		Reduct(*this);
+		return *this;
 	}
 
-};
+	Fraction& operator-=(Fraction rhs) {
+			if (denominator == rhs.denominator) {
+				numerator -= rhs.numerator;
+			}
+			else {
+				uint64_t tmp = gcd(denominator, rhs.denominator);
+					numerator *= tmp / denominator;
+					rhs.numerator *= tmp / rhs.denominator;
+					numerator -= rhs.numerator;
+					denominator = tmp;
+				}
+			Reduct(*this);
+			return *this;
+	}
 
-int main() {
-	Fraction x(5, 1), y(5, 4), c(1, 1);
-	x += y;
-	std::cout << "x is " << x.numerator << ":" << x.denominator << ", y is " << y.numerator <<":" << y.denominator
-			<< ", c is " << c.numerator << ":" << c.denominator << "\n";
-return 0;
-}
+	Fraction& operator*=(Fraction rhs) {
+		numerator *= rhs.numerator;
+		denominator *= rhs.denominator;
+		Reduct(*this);
+		return *this;
+	}
+};
