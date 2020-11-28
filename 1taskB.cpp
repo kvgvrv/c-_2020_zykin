594c720c4be70e310a673e348826dcc55e4161a3
kvgvrv <71896722+kvgvrv@users.noreply.github.com>
2020-11-28 19:21:10 +0300

    Add files via upload

---
+++ 1taskB.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+
+struct branch {
+	int value;
+	int left;
+	int right;
+};
+
+bool DFS(std::vector<branch>& tree, int i, int big, int smoll) {
+	if (tree[i].left != -1) {
+		if (tree[tree[i].left].value > tree[i].value or tree[tree[i].left].value < smoll) {
+			return 0;
+		}
+		else {
+			return DFS(tree, tree[i].left, tree[i].value, smoll);	//если заходим в левый узел, меняем верхнюю правую границу
+		}
+	}
+	if (tree[i].right != -1) {		//если заходим в правый узел, меняем нижнюю границу
+		if (tree[tree[i].right].value < tree[i].value or tree[tree[i].right].value > big) {
+			return 0;
+		}
+		else {
+			return DFS(tree, tree[i].right, big, tree[i].value);
+		}
+	}
+	if (i == 0) {
+		return 1;
+	}
+}
+
+int main() {
+	int N;
+	std::cin >> N;
+	std::vector<branch> tree(N);
+	for (int i = 0; i < N; ++i) {
+		int v, l, r;
+		std::cin >> v >> l >> r;
+		tree[i].value = v;
+		tree[i].left = l;
+		tree[i].right = r;
+	}
+	if (DFS(tree, 0, 2000000000, -2000000000)) {
+		std::cout << "YES";
+	}
+	else {
+		std::cout << "NO";
+	}
+	return 0;
+}
---
+++ 1taskC.cpp
@@ -0,0 +1,105 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+
+
+class BinaryTree {
+
+public:
+	int value;
+	BinaryTree* left_child;
+	BinaryTree* right_child;
+	unsigned int height;
+	unsigned int depth;
+
+	BinaryTree() {
+		left_child = NULL;
+		right_child = NULL;
+		value = 0;
+		height = 0;
+		depth = 0;
+	}
+
+	BinaryTree(int val) {
+		value = val;
+		left_child = NULL;
+		right_child = NULL;
+		height = 1;
+		depth = 0;
+	}
+
+	~BinaryTree() {}
+	//метод вставляющий в дерево новый элемент
+	void insert(int val) {
+		if (val < value) {
+			if (left_child == NULL) {
+				BinaryTree* new_point = new BinaryTree(val);
+				left_child = new_point;
+			}
+			else {
+				left_child->insert(val);
+			}
+		}
+		else {
+			if (right_child == NULL) {
+				BinaryTree* new_point = new BinaryTree(val);
+				right_child = new_point;
+			}
+			else {
+				right_child->insert(val);
+			}
+		}
+	}
+
+	void print_height() {
+		std::cout << height << "\n";
+	}
+};
+//обхожу в глубь дерево, вычисляю глубину каждого узла, каждый раз когда доходим до узла,
+//у котрого нет детей, сравниваем его глубину с текущей высотой дерева, если она больше, меняем высоту
+void DFS(BinaryTree* branch, BinaryTree* root) {
+	if (branch->left_child != NULL) {
+		(branch->left_child)->depth = branch->depth + 1;
+		DFS(branch->left_child, root);
+	}
+	if (branch->right_child != NULL) {
+		(branch->right_child)->depth = branch->depth + 1;
+		DFS(branch->right_child, root);
+	}
+	if (branch->depth > root->height) {
+		root->height = branch->depth + 1;
+	}
+}
+
+int main() {
+	std::string line;
+	std::vector<int> data;
+	//считываем строку последовательности чисел
+	getline(std::cin, line);
+	std::string v = "";
+	int x;
+	for (const auto& c : line) {
+		if (c != ' ') {
+			v += c;
+		}
+		else {
+			x = stoi(v);
+			v = "";
+			data.push_back(x);
+		}
+	}
+	if (v != "") {
+		x = stoi(v);
+		data.push_back(x);
+	}
+	//создаем корень и заполняем дерево
+	BinaryTree root(data[0]);
+	for (int i = 1; i < data.size(); ++i) {
+		root.insert(data[i]);
+	}
+	//выполняем функцию вычисления высоты и печатаем ее
+	DFS(&root, &root);
+	root.print_height();
+	return 0;
+}
